c7c74f6b2102bf61ebbceb27d3fa3fcee8dbe91b
Luciano Eduardo <luciano123ab@gmail.com>
2026-08-03 17:08:47 -0300

    004(VerLogs_ProducaoForma1)

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    Auth::loginUsingId(4);
+    // Auth::logout();
+
     // throw new Exception('This is a test exception');
-    Log::channel('custom')->info('This is a test log message');
+    // Log::channel('custom')->info('This is a test log message');
 
     return view('welcome');
 });
